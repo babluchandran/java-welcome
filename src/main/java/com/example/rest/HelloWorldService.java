48c89abd332b82c1464a221c9c6e3ca2441e1e0e
babluchandran <babluchandran@users.noreply.github.com>
2017-04-12 16:49:53 +0530

    Update HelloWorldService.java

--- src/main/java/com/example/rest/HelloWorldService.java
+++ src/main/java/com/example/rest/HelloWorldService.java
@@ -39,7 +39,7 @@ public class HelloWorldService {
     @Path("/")
     @GET
     public String home(){
-        return "Hello Techm from "+ hostname;
+        return "Hello Bablu from "+ hostname;
     }
 
     @Path("/healthz")
